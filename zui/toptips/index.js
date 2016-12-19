8e9c71ebd173a843aa79dcaf4b7ca1c420369441
pangxie1991 <chenyao1102@gmail.com>
2016-12-19 17:25:03 +0800

    toptips

---
+++ zui/toptips/index.js
@@ -0,0 +1,38 @@
+var timer = undefined;
+
+module.exports = {
+  showZuiTopTips(content = '', options = {}) {
+    // 如果已经有一个计时器在了，就清理掉先
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    if (typeof options === 'number') {
+      options = {
+        duration: options
+      };
+    }
+
+    // options参数默认参数扩展
+    options = Object.assign({
+      duration: 3000
+    }, options);
+
+    // 展示出topTips
+    this.setData({
+      componentTopTips: {
+        show: true,
+        content,
+        options
+      }
+    });
+
+    // 设置定时器，定时关闭topTips
+    timer = setTimeout(() => {
+      this.setData({
+        'componentTopTips.show': false
+      });
+      timer = undefined;
+    }, options.duration);
+  }
+};

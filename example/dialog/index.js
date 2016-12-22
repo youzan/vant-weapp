9b761bd032304d1e3efeb2e98ea9c89fcfd2c390
Nino <ninofocus@gmail.com>
2016-12-22 15:00:10 +0800

    add dialog example

---
+++ example/dialog/index.js
@@ -0,0 +1,20 @@
+var app = getApp()
+
+Page({
+  data: {
+    showDialog: false
+  },
+
+  toggleDialog() {
+    this.setData({
+      showDialog: !this.data.showDialog
+    });
+  },
+
+  onLoad: function () {
+
+  },
+
+  onShow: function() {
+  },
+})

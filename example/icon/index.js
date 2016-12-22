6cded1d97b4b1b8d7f8e7dcd17ff9bd28d47b9b0
Nino <ninofocus@gmail.com>
2016-12-22 10:48:50 +0800

    add icon

---
+++ example/icon/index.js
@@ -0,0 +1,21 @@
+var app = getApp()
+
+Page({
+  data: {
+    icons: [
+      'cancel',
+      'store',
+      'certificate',
+      'arrow',
+      'shopping-cart',
+
+    ]
+  },
+
+  onLoad: function () {
+
+  },
+
+  onShow: function() {
+  },
+})

3b934df32e9687ce44f4e9f5750f7ed45360b15c
Yao <chenyao1102@gmail.com>
2017-10-15 22:38:14 +0800

    add release shell (#35)

---
+++ build/releash.sh
@@ -0,0 +1,24 @@
+git checkout master
+
+#!/usr/bin/env sh
+set -e
+echo "Enter release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing $VERSION ..."
+
+  # commit
+  git add -A
+  git commit -m "[build] $VERSION"
+  npm version $VERSION --message "[release] $VERSION"
+
+  # publish
+  git push origin master
+  git push origin refs/tags/v$VERSION
+
+  npm publish
+fi

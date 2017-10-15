fb2e85f3c824dbccdef1961de44d8c63b9b08226
pangxie1991 <chenyao1102@gmail.com>
2017-10-15 21:08:14 +0800

    整理文档

---
+++ example/dashboard/config.js
@@ -0,0 +1,74 @@
+export default {
+  base: {
+    title: '基础组件',
+    content: [
+      {
+        name: 'Badge 徽章',
+        path: '/example/badge/index'
+      }, {
+        name: 'Button 按钮',
+        path: '/example/btn/index'
+      }, {
+        name: 'Capsule 胶囊',
+        path: '/example/capsule/index'
+      }, {
+        name: 'Card 卡片',
+        path: '/example/card/index'
+      }, {
+        name: 'Cell 单元格',
+        path: '/example/cell/index'
+      }, {
+        name: 'Form 表单',
+        path: '/example/form/index'
+      }, {
+        name: 'Helper 基础样式',
+        path: '/example/helper/index'
+      }, {
+        name: 'Icon 图标',
+        path: '/example/icon/index'
+      }, {
+        name: 'Label 标签',
+        path: '/example/label/index'
+      }, {
+        name: 'Loadmore 加载',
+        path: '/example/loadmore/index'
+      }, {
+        name: 'Noticebar 通告栏',
+        path: '/example/noticebar/index'
+      }, {
+        name: 'Panel 面板',
+        path: '/example/panel/index'
+      }, {
+        name: 'Popup 弹出层',
+        path: '/example/popup/index'
+      }, {
+        name: 'Quantity 计数器',
+        path: '/example/quantity/index'
+      }, {
+        name: 'Steps 步骤条',
+        path: '/example/steps/index'
+      }, {
+        name: 'Switch 开关',
+        path: '/example/switch/index'
+      }, {
+        name: 'Tab 标签',
+        path: '/example/tab/index'
+      }, {
+        name: 'Toptips 顶部提示',
+        path: '/example/toptips/index'
+      }
+    ]
+  },
+  action: {
+    title: '操作反馈',
+    content: [
+      {
+        name: 'Toast 轻提示',
+        path: '/example/toast/index'
+      }, {
+        name: 'Dialog 弹出框',
+        path: '/example/dialog/index'
+      }
+    ]
+  }
+}

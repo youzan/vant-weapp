9c05bd2d5857a0bcbf6b223c2b851d64c5c9a723
Yao <chenyao1102@gmail.com>
2017-11-17 21:13:35 +0800

    [new feature] Field:  增加 value 传入支持 (#60)
    * field 支持 value 传入
    
    * textarea 清除输入示例

--- example/pages/field/index.js
+++ example/pages/field/index.js
@@ -2,6 +2,8 @@ var Zan = require('../../dist/index');
 
 Page(Object.assign({}, Zan.Field, {
   data: {
+    value: 'test',
+    textareaValue: 'test textarea',
     area: ['省份', '北京市', '天津市', '河北省', '山西省', '内蒙古自治区', '辽宁省', '吉林省', '黑龙江省', '上海市', '江苏省', '浙江省', '安徽省', '福建省', '江西省', '山东省', '河南省', '湖北省', '湖南省', '广东省', '广西壮族自治区', '海南省', '重庆市', '四川省', '贵州省', '云南省', '西藏自治区', '陕西省', '甘肃省', '青海省', '宁夏回族自治区', '新疆维吾尔自治区', '台湾省', '香港特别行政区', '澳门特别行政区'],
     areaIndex: 0
   },
@@ -16,5 +18,17 @@ Page(Object.assign({}, Zan.Field, {
     const { componentId, detail } = e;
 
     console.log('[zan:field:change]', componentId, detail);
+  },
+
+  clearInput() {
+    this.setData({
+      value: ''
+    });
+  },
+
+  clearTextarea() {
+    this.setData({
+      textareaValue: ''
+    });
   }
 }));

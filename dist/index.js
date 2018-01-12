8a762b3ecee453f27bc01a8cd5286d7496519f76
pangxie1991 <chenyao1102@gmail.com>
2018-01-12 22:07:51 +0800

    [build] 2.4.4

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,16 @@
-exports.CheckLabel = require('./select/index');
+exports.Actionsheet = require('./actionsheet/index');
 exports.Dialog = require('./dialog/index');
 exports.Field = require('./field/index');
 exports.NoticeBar = require('./noticebar/index');
+exports.Select = require('./select/index');
 exports.Stepper = require('./stepper/index');
 exports.Switch = require('./switch/index');
 exports.Tab = require('./tab/index');
 exports.Toast = require('./toast/index');
 exports.TopTips = require('./toptips/index');
+
+// 兼容老版本，在下次大版本发布时会被移除
+exports.CheckLabel = require('./select/index');
 
 const { extend } = require('./common/helper');
 exports.extend = extend;

ccc40e843097339793a28ad40fa9455d0a7ed6a5
liujiandong <liujiandong@youzan.com>
2018-02-06 00:09:41 +0800

    travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,8 @@
-npm run build
-
-cd dist
 git config --global user.name "Travis CI"
 git config --global user.email "ci@travis-ci.org"
 
-$(npm bin)/update-branch --commands "npm run build-travis" \
+$(npm bin)/update-branch --commands "npm run build" \
                          --commit-message "Update gh-pages [skip ci]" \
-                         --directory "website/dist" \
+                         --directory "dist" \
                          --distribution-branch "gh-pages" \
                          --source-branch "master"

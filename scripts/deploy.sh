6c389e226a7de4c2fc79d40dbbf5db267d61851e
liujiandong <liujiandong@youzan.com>
2018-02-12 14:51:55 +0800

    gh-pages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,6 @@ cd website && yarn && yarn cache node_modules
 
 npm run build
 
-cd dist && git init
+cd ../
 
-NAME=`git config user.name`
-EMAIL=`git config user.email`
-git config --global user.name $NAME
-git config --global user.email $EMAIL
-git add -A && git commit -am "Update gh-pages [skip ci]"
 

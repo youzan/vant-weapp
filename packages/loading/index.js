9a6519c52c0f45c2d88e6f843e6e3ac308b1ac8c
liujiandong <liujiandong@youzan.com>
2018-03-31 22:31:15 +0800

    refactor: loading

---
+++ packages/loading/index.js
@@ -0,0 +1,15 @@
+Component({
+  properties: {
+    type: {
+      type: String,
+      value: 'circle'
+    },
+    color: {
+      type: String
+    },
+    use: {
+      type: null,
+      value: 1
+    }
+  }
+})

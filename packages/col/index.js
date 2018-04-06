2457839b1e37f7785fb0b984e282dad0413858f2
niunai <niunai@youzan.com>
2018-04-07 00:01:07 +0800

    layout重构

---
+++ packages/col/index.js
@@ -0,0 +1,14 @@
+Component({
+  externalClasses: ['col-class'],
+
+  properties: {
+    col: {
+      value: 0,
+      type: Number
+    },
+    offset: {
+      value: 0,
+      type: Number
+    }
+  }
+});
---
+++ packages/row/index.js
@@ -0,0 +1,3 @@
+Component({
+  externalClasses: ['row-class']
+});

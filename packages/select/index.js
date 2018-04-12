71f92fdd9810f0da879103bfefa96670e2b37cd5
zhuxiang <zhuxiang@youzan.com>
2018-04-12 16:24:53 +0800

    done

--- packages/select/index.js
+++ packages/select/index.js
@@ -3,6 +3,18 @@ Component({
     items: {
       type: Array,
       value: []
+    },
+    name: {
+      type: String,
+      value: ''
+    },
+    checkedValue: {
+      type: String,
+      value: ''
+    },
+    activeColor: {
+      type: String,
+      value: '#ff4444'
     }
   },
 

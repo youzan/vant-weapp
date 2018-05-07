a4414fe9634c3a2fe89f77930dfb8cc72688a8f0
pangxie1991 <pangxie@youzan.com>
2018-05-07 22:57:11 +0800

    toast 增强

--- packages/toast/toast.js
+++ packages/toast/toast.js
@@ -1,15 +1,39 @@
+const TOAST_CONFIG_KEY = 'zanui.__zanToastPageConfig';
+
 let timeoutData = {
   timeoutId: 0,
   toastCtx: null
 };
 
-function Toast(options = {}, pageCtx) {
+let globalToastUserConfig = {};
+
+// 获取页面上下文
+function getPageCtx(pageCtx) {
   let ctx = pageCtx;
+
   if (!ctx) {
     const pages = getCurrentPages();
     ctx = pages[pages.length - 1];
   }
-  const toastCtx = ctx.selectComponent(options.selector);
+
+  return ctx;
+}
+
+// 处理 toast 参数格式化
+function parseToastOptions(options, pageCtx) {
+
+}
+
+// Toast 显示函数
+function Toast(options = {}, pageCtx) {
+  let ctx = getPageCtx(pageCtx);
+  const pageToastUserSetting = (ctx.data.zanui || {}).__zanToastPageConfig || {}
+  const parsedOptions = {
+    ...globalToastUserConfig,
+    ...pageToastUserSetting,
+    options
+  };
+  const toastCtx = ctx.selectComponent(parsedOptions.selector);
 
   if (!toastCtx) {
     console.error('无法找到对应的toast组件，请于页面中注册并在 wxml 中声明 toast 自定义组件');
@@ -21,19 +45,53 @@ function Toast(options = {}, pageCtx) {
   }
 
   toastCtx.show({
-    ...options,
+    ...parsedOptions,
     show: true
   });
 
   const timeoutId = setTimeout(() => {
     toastCtx.clear();
-  }, options.timeout || 3000);
+  }, parsedOptions.timeout || 3000);
 
   timeoutData = {
     timeoutId,
     toastCtx
   };
 }
+
+// 设置 toast 基础属性
+Toast.setDefaultOptions = function (options = {}, type = 'global') {
+  const parsedDefaultOptions = {
+    selector: options.selector || '',
+    type: options.type || '',
+    icon: options.icon || '',
+    image: options.image || '',
+    timeout: options.timeout || 3000
+  };
+
+  if (type === 'global') {
+    globalToastUserConfig = {
+      ...parsedDefaultOptions
+    };
+  } else {
+    let ctx = getPageCtx();
+    ctx.setData({
+      [`${TOAST_CONFIG_KEY}`]: parsedDefaultOptions
+    });
+  }
+};
+
+// 重置 toast 基础属性
+Toast.resetDefaultOptions = function (type = 'global') {
+  if (type === 'global') {
+    globalToastUserConfig = {};
+  } else {
+    let ctx = getPageCtx();
+    ctx.setData({
+      [`${TOAST_CONFIG_KEY}`]: {}
+    });
+  }
+};
 
 // 清理所有 toast
 Toast.clear = function () {

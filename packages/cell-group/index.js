542d702725176d346171f135ee14f64bc9a5a2bc
pangxie1991 <pangxie@youzan.com>
2018-05-11 14:11:04 +0800

    修复cell边框在元素更新后，会不显示的问题

--- packages/cell-group/index.js
+++ packages/cell-group/index.js
@@ -1,18 +1,39 @@
+let cellUpdateTimeout = 0;
+
 Component({
   relations: {
     '../cell/index': {
       type: 'child',
-      linked() {}
+      linked() {
+        this._updateIsLastCell();
+      },
+      linkChanged() {
+        this._updateIsLastCell();
+      },
+      unlinked() {
+        this._updateIsLastCell();
+      }
     }
   },
-  ready() {
-    let cells = this.getRelationNodes('../cell/index');
 
-    if (cells.length > 0) {
-      let lastIndex = cells.length - 1;
+  methods: {
+    _updateIsLastCell() {
+      // 用 setTimeout 减少计算次数
+      if (cellUpdateTimeout > 0) {
+        return;
+      }
 
-      cells.forEach((cell, index) => {
-        if (index < lastIndex) cell.notLastCell();
+      cellUpdateTimeout = setTimeout(() => {
+        cellUpdateTimeout = 0;
+        let cells = this.getRelationNodes('../cell/index');
+
+        if (cells.length > 0) {
+          let lastIndex = cells.length - 1;
+
+          cells.forEach((cell, index) => {
+            cell.updateIsLastCell(index === lastIndex);
+          });
+        }
       });
     }
   }

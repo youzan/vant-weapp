15aaf8bee022818f847ca5b523bb6575d0777e96
nirizhe <nirizhe@youzan.com>
2018-07-01 22:50:40 +0800

    [new component]: Checkbox refactor

--- packages/checkbox/index.js
+++ packages/checkbox/index.js
@@ -1,27 +1,16 @@
 Component({
   properties: {
-    items: {
-      type: Array,
-      value: []
-    },
-    name: {
-      type: String,
-      value: ''
-    },
-    checkedValue: {
-      type: String,
-      value: ''
-    },
-    activeColor: {
-      type: String,
-      value: '#ff4444'
+    checked: {
+      type: Boolean,
+      value: true
     }
   },
 
   methods: {
-    handleCheckboxChange(e) {
-      const value = e.detail.value;
-      this.triggerEvent('change', { value });
+    handleClick() {
+      this.setData({
+        checked: !this.data.checked
+      })
     }
   }
 });

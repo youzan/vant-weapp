aad3293e8e7e4aef87e09176b44e199c1bcb64fc
rex-zsd <rexkaneki@gmail.com>
2018-07-08 19:30:37 +0800

    feat(tab): tab组件支持选中项自动滑动到中间位置 @rex-zsd
    #307

--- packages/tab/index.js
+++ packages/tab/index.js
@@ -24,9 +24,28 @@ Component({
     }
   },
 
+  data: {
+    scrollLeft: 0
+  },
+
   methods: {
+    _handleScroll(selectedId) {
+      const query = wx.createSelectorQuery().in(this);
+      query.select(`#item-${selectedId}`).boundingClientRect();
+      query.select('#scroll-view').boundingClientRect();
+      query.select('#scroll-view').scrollOffset();
+      query.exec(res => {
+        this.setData({
+          scrollLeft: res[2].scrollLeft + res[0].left + res[0].width / 2 - res[1].width / 2
+        });
+      });
+    },
     _handleZanTabChange(e) {
       const selectedId = e.currentTarget.dataset.itemId;
+
+      if (this.data.scroll) {
+        this._handleScroll(selectedId);
+      }
 
       this.setData({
         selectedId

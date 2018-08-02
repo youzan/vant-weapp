3fb3ccad341edf680933e61ce4cbb35a78eda143
陈嘉涵 <chenjiahan@youzan.com>
2018-08-02 16:26:22 +0800

    [Doc] update mobile nav

--- example/pages/dashboard/config.js
+++ example/pages/dashboard/config.js
@@ -3,50 +3,60 @@ export default {
     title: '基础组件',
     content: [
       {
-        name: 'Badge 徽章',
-        path: '/pages/badge/index'
-      }, {
-        name: 'Button 按钮',
-        path: '/pages/button/index'
-      }, {
-        name: 'Card 卡片',
-        path: '/pages/card/index'
-      }, {
-        name: 'Cell 单元格',
-        path: '/pages/cell/index'
-      }, {
-        name: 'Helper 基础样式',
-        path: '/pages/helper/index'
-      }, {
-        name: 'Icon 图标',
-        path: '/pages/icon/index'
-      }, {
         name: 'Layout 布局',
         path: '/pages/layout/index'
-      }, {
-        name: 'loading 加载',
+      },
+      {
+        name: 'Base 基础样式',
+        path: '/pages/helper/index'
+      },
+      {
+        name: 'Badge 徽章',
+        path: '/pages/badge/index'
+      },
+      {
+        name: 'Button 按钮',
+        path: '/pages/button/index'
+      },
+      {
+        name: 'Cell 单元格',
+        path: '/pages/cell/index'
+      },
+      {
+        name: 'Icon 图标',
+        path: '/pages/icon/index'
+      },
+      {
+        name: 'Loading 加载',
         path: '/pages/loading/index'
-      }, {
-        name: 'NoticeBar 通告栏',
-        path: '/pages/notice-bar/index'
-      }, {
-        name: 'Panel 面板',
-        path: '/pages/panel/index'
-      }, {
-        name: 'Popup 弹出层',
-        path: '/pages/popup/index'
-      }, {
-        name: 'Steps 步骤条',
-        path: '/pages/steps/index'
-      }, {
-        name: 'Tab 标签',
-        path: '/pages/tab/index'
-      }, {
-        name: 'Tag 标记',
-        path: '/pages/tag/index'
-      }, {
+      },
+      {
         name: 'Loadmore 加载更多',
         path: '/pages/loadmore/index'
+      },
+      {
+        name: 'NoticeBar 通告栏',
+        path: '/pages/notice-bar/index'
+      },
+      {
+        name: 'Panel 面板',
+        path: '/pages/panel/index'
+      },
+      {
+        name: 'Popup 弹出层',
+        path: '/pages/popup/index'
+      },
+      {
+        name: 'Steps 步骤条',
+        path: '/pages/steps/index'
+      },
+      {
+        name: 'Tab 标签',
+        path: '/pages/tab/index'
+      },
+      {
+        name: 'Tag 标记',
+        path: '/pages/tag/index'
       }
     ]
   },
@@ -56,22 +66,24 @@ export default {
       {
         name: 'Checkbox 复选框',
         path: '/pages/checkbox/index'
-      }, {
-        name: 'Datetime 时间选择器',
-        path: '/pages/datetime/index'
-      }, {
+      },
+      {
         name: 'Field 输入框',
         path: '/pages/field/index'
-      }, {
+      },
+      {
         name: 'Radio 单选框',
         path: '/pages/radio/index'
-      }, {
+      },
+      {
         name: 'Search 搜索',
         path: '/pages/search/index'
-      }, {
+      },
+      {
         name: 'Stepper 步进器',
         path: '/pages/stepper/index'
-      }, {
+      },
+      {
         name: 'Switch 开关',
         path: '/pages/switch/index'
       }
@@ -83,15 +95,31 @@ export default {
       {
         name: 'Actionsheet 上拉菜单',
         path: '/pages/actionsheet/index'
-      }, {
+      },
+      {
         name: 'Dialog 弹出框',
         path: '/pages/dialog/index'
-      }, {
+      },
+      {
+        name: 'DatetimePicker 时间选择器',
+        path: '/pages/datetime/index'
+      },
+      {
         name: 'Toast 轻提示',
         path: '/pages/toast/index'
-      }, {
+      },
+      {
         name: 'TopTips 顶部提示',
         path: '/pages/toptips/index'
+      }
+    ]
+  },
+  bussiness: {
+    title: '业务组件',
+    content: [
+      {
+        name: 'Card 卡片',
+        path: '/pages/card/index'
       }
     ]
   }

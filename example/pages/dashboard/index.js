0800bfa02363da89545e402d9c144801ead5c8f6
陈嘉涵 <chenjiahan@youzan.com>
2018-08-13 14:00:46 +0800

    [Improvement] add utils

--- example/pages/dashboard/index.js
+++ example/pages/dashboard/index.js
@@ -1,14 +1,8 @@
-import config from '../../config';
+import list from '../../config';
 import Page from '../../common/page';
 
 Page({
   data: {
-    list: config
-  },
-
-  onShareAppMessage() {
-    return {
-      title: 'Vant Weapp 组件库演示'
-    };
+    list
   }
 });

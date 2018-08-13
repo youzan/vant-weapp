b9576eee0d7b0852f27d0ef387c150a5c210bc0f
neverland <chenjiahan@buaa.edu.cn>
2018-08-13 10:11:17 +0800

    [Doc] reduce config file (#412)

--- docs/src/doc.config.js
+++ docs/src/doc.config.js
@@ -1,7 +1,9 @@
 /* eslint-disable */
-const version = require('../../package.json').version;
+import packageJson from '../../package.json';
+import components from '../../example/config';
+const { version } = packageJson;
 
-module.exports = {
+export default {
   header: {
     logo: {
       image: 'https://img.yzcdn.cn/public_files/2017/12/18/fd78cf6bb5d12e2a119d0576bedfd230.png',
@@ -43,113 +45,7 @@ module.exports = {
     },
     {
       name: '组件',
-      groups: [
-        {
-          groupName: '基础组件',
-          list: [
-            {
-              path: '/col',
-              title: 'Layout 布局'
-            },
-            {
-              path: '/badge',
-              title: 'Badge 徽章'
-            },
-            {
-              path: '/button',
-              title: 'Button 按钮'
-            },
-            {
-              path: '/cell',
-              title: 'Cell 单元格'
-            },
-            {
-              path: '/icon',
-              title: 'Icon 图标'
-            },
-            {
-              path: '/loading',
-              title: 'Loading 加载'
-            },
-            {
-              path: '/nav-bar',
-              title: 'NavBar 导航栏'
-            },
-            {
-              path: '/notice-bar',
-              title: 'NoticeBar 通告栏'
-            },
-            {
-              path: '/panel',
-              title: 'Panel 面板'
-            },
-            {
-              path: '/popup',
-              title: 'Popup 弹出层'
-            },
-            {
-              path: '/steps',
-              title: 'Steps 步骤条'
-            },
-            {
-              path: '/tag',
-              title: 'Tag 标记'
-            }
-          ]
-        },
-        {
-          groupName: '表单组件',
-          list: [
-            {
-              path: '/field',
-              title: 'Field 输入框'
-            },
-            {
-              path: '/search',
-              title: 'Search 搜索'
-            },
-            {
-              path: '/stepper',
-              title: 'Stepper 步进器'
-            },
-            {
-              path: '/switch',
-              title: 'Switch 开关'
-            }
-          ]
-        },
-        {
-          groupName: '操作反馈',
-          list: [
-            {
-              path: '/actionsheet',
-              title: 'Actionsheet 上拉菜单'
-            },
-            {
-              path: '/notify',
-              title: 'Notify 消息通知'
-            }
-          ]
-        },
-        {
-          groupName: '高阶组件',
-          list: [
-            {
-              path: '/tree-select',
-              title: 'TreeSelect 分类选择'
-            }
-          ]
-        },
-        {
-          groupName: '业务组件',
-          list: [
-            {
-              path: '/card',
-              title: 'Card 卡片'
-            }
-          ]
-        }
-      ]
+      groups: components
     }
   ]
 };
---
+++ example/config.js
@@ -0,0 +1,111 @@
+export default [
+  {
+    groupName: '基础组件',
+    list: [
+      {
+        path: '/col',
+        title: 'Layout 布局'
+      },
+      {
+        path: '/badge',
+        title: 'Badge 徽章'
+      },
+      {
+        path: '/button',
+        title: 'Button 按钮'
+      },
+      {
+        path: '/cell',
+        title: 'Cell 单元格'
+      },
+      {
+        path: '/icon',
+        title: 'Icon 图标'
+      },
+      {
+        path: '/loading',
+        title: 'Loading 加载'
+      },
+      {
+        path: '/nav-bar',
+        title: 'NavBar 导航栏'
+      },
+      {
+        path: '/notice-bar',
+        title: 'NoticeBar 通告栏'
+      },
+      {
+        path: '/panel',
+        title: 'Panel 面板'
+      },
+      {
+        path: '/popup',
+        title: 'Popup 弹出层'
+      },
+      {
+        path: '/steps',
+        title: 'Steps 步骤条'
+      },
+      {
+        path: '/tag',
+        title: 'Tag 标记'
+      }
+    ]
+  },
+  {
+    groupName: '表单组件',
+    list: [
+      {
+        path: '/field',
+        title: 'Field 输入框'
+      },
+      {
+        path: '/search',
+        title: 'Search 搜索'
+      },
+      {
+        path: '/stepper',
+        title: 'Stepper 步进器'
+      },
+      {
+        path: '/switch',
+        title: 'Switch 开关'
+      }
+    ]
+  },
+  {
+    groupName: '操作反馈',
+    list: [
+      {
+        path: '/actionsheet',
+        title: 'Actionsheet 上拉菜单'
+      },
+      {
+        path: '/notify',
+        title: 'Notify 消息通知'
+      },
+      {
+        path: '/toast',
+        title: 'Toast 轻提示'
+      }
+    ]
+  },
+  {
+    groupName: '高阶组件',
+    list: [
+      {
+        path: '/tree-select',
+        title: 'TreeSelect 分类选择'
+      }
+    ]
+  },
+  {
+    groupName: '业务组件',
+    list: [
+      {
+        path: '/card',
+        title: 'Card 卡片'
+      }
+    ]
+  }
+];

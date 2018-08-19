f6e25a26db4944efe7e6c835e170f78268393d33
neverland <chenjiahan@buaa.edu.cn>
2018-08-19 10:41:46 +0800

    [new feature] add Toast component (#437)

---
+++ packages/toast/index.js
@@ -0,0 +1,32 @@
+import Toast from './toast';
+
+Component({
+  properties: {
+    show: Boolean,
+    mask: Boolean,
+    message: String,
+    forbidClick: Boolean,
+    type: {
+      type: String,
+      value: 'text'
+    },
+    loadingType: {
+      type: String,
+      value: 'circular'
+    },
+    position: {
+      type: String,
+      value: 'middle'
+    }
+  },
+
+  methods: {
+    clear() {
+      this.setData({
+        show: false
+      });
+    }
+  }
+});
+
+export default Toast;

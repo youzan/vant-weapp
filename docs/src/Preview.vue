b850674edf03ea6d51bccf5c57ad3dc5771cf01e
陈嘉涵 <chenjiahan@youzan.com>
2018-08-19 10:47:47 +0800

    [Doc] add toast preview

--- docs/src/Preview.vue
+++ docs/src/Preview.vue
@@ -34,6 +34,7 @@ const MAP = {
   switch: 'switch-201808092138.png',
   tag: 'tag-201808092138.png',
   tabbar: 'tabbar-201808160922.png',
+  toast: 'toast-201808191046.png',
   'tree-select': 'tree-select-201808092138.png'
 };
 

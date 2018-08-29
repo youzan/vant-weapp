b47c5dbbee0362dd2d30c9828201350e9c274d47
陈嘉涵 <chenjiahan@youzan.com>
2018-08-29 16:44:23 +0800

    [improvement] SwitchCell: support form-filed

--- dist/switch-cell/index.js
+++ dist/switch-cell/index.js
@@ -1,18 +1,29 @@
 Component({
+  behaviors: ['wx://form-field'],
+
   options: {
     addGlobalClass: true
   },
 
   properties: {
     title: String,
-    checked: Boolean,
     border: Boolean,
     loading: Boolean,
     disabled: Boolean,
+    checked: {
+      type: Boolean,
+      observer(value) {
+        this.setData({ value });
+      }
+    },
     size: {
       type: String,
       value: '26px'
     }
+  },
+
+  attached() {
+    this.setData({ value: this.data.checked });
   },
 
   methods: {

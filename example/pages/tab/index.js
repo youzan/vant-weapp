216e4eb8e6c84d67ccdfb203ac2f242d32f143d0
neverland <chenjiahan@buaa.edu.cn>
2018-09-05 11:06:59 +0800

    [new feature] add Tab component (#496)

---
+++ example/pages/tab/index.js
@@ -0,0 +1,29 @@
+import Page from '../../common/page';
+
+Page({
+  data: {
+    tabs: [1, 2, 3, 4],
+    tabsMore: [1, 2, 3, 4, 5, 6, 7, 8]
+  },
+
+  onClickDisabled(event) {
+    wx.showToast({
+      title: `标签 ${event.detail.index + 1} 已被禁用`,
+      icon: 'none'
+    });
+  },
+
+  onChange(event) {
+    wx.showToast({
+      title: `切换到标签 ${event.detail.index + 1}`,
+      icon: 'none'
+    });
+  },
+
+  onClick(event) {
+    wx.showToast({
+      title: `点击标签 ${event.detail.index + 1}`,
+      icon: 'none'
+    });
+  }
+});

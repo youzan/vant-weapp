57f8e54b198f69d610c0fabac9f51787e9621c1f
neverland <chenjiahan@buaa.edu.cn>
2018-09-05 14:33:18 +0800

    [Improvement] Popup: support leave animation (#498)

---
+++ packages/behaviors/transition.js
@@ -0,0 +1,52 @@
+module.exports = Behavior({
+  properties: {
+    customStyle: String,
+    show: {
+      value: true,
+      type: Boolean,
+      observer(value) {
+        if (value) {
+          this.show();
+        } else {
+          this.setData({
+            type: 'leave'
+          });
+        }
+      }
+    },
+    duration: {
+      type: Number,
+      value: 300
+    }
+  },
+
+  data: {
+    type: '',
+    inited: false,
+    display: false
+  },
+
+  attached() {
+    if (this.data.show) {
+      this.show();
+    }
+  },
+
+  methods: {
+    show() {
+      this.setData({
+        inited: true,
+        display: true,
+        type: 'enter'
+      });
+    },
+
+    onAnimationEnd() {
+      if (!this.data.show) {
+        this.setData({
+          display: false
+        });
+      }
+    }
+  }
+});

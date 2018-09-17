8ab19338296e7c6a5cae50130fbda628f90eaf3f
neverland <chenjiahan@buaa.edu.cn>
2018-09-17 21:57:24 +0800

    [build] add tsconfig.json (#588)

--- build/compiler.js
+++ build/compiler.js
@@ -4,10 +4,12 @@ const ts = require('gulp-typescript');
 const postcss = require('gulp-postcss');
 const cssmin = require('gulp-clean-css');
 const rename = require('gulp-rename');
+
 const isProduction = process.env.NODE_ENV === 'production';
 const src = path.join(__dirname, '../packages');
 const dist = path.join(__dirname, isProduction ? '../dist' : '../example/dist');
 const ext = ['js', 'ts', 'pcss', 'json', 'wxml'];
+const tsProject = ts.createProject('tsconfig.json');
 
 function copy(ext) {
   return gulp.src([src + '/**/*.' + ext]).pipe(gulp.dest(dist));
@@ -30,7 +32,7 @@ gulp.task('compile-js', () => copy('js'));
 gulp.task('compile-ts', () =>
   gulp
     .src([src + '/**/*.ts'])
-    .pipe(ts())
+    .pipe(tsProject())
     .pipe(gulp.dest(dist))
 );
 gulp.task('compile-json', () => copy('json'));

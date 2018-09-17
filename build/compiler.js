ed51c2608e4bc658c77b465db874db7f045f9282
neverland <chenjiahan@buaa.edu.cn>
2018-09-17 21:43:29 +0800

    [build] support compile typescript file (#587)

--- build/compiler.js
+++ build/compiler.js
@@ -1,12 +1,13 @@
 const gulp = require('gulp');
 const path = require('path');
+const ts = require('gulp-typescript');
 const postcss = require('gulp-postcss');
 const cssmin = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const isProduction = process.env.NODE_ENV === 'production';
 const src = path.join(__dirname, '../packages');
 const dist = path.join(__dirname, isProduction ? '../dist' : '../example/dist');
-const ext = ['js', 'pcss', 'json', 'wxml'];
+const ext = ['js', 'ts', 'pcss', 'json', 'wxml'];
 
 function copy(ext) {
   return gulp.src([src + '/**/*.' + ext]).pipe(gulp.dest(dist));
@@ -26,6 +27,12 @@ gulp.task('compile-pcss', () => {
 });
 
 gulp.task('compile-js', () => copy('js'));
+gulp.task('compile-ts', () =>
+  gulp
+    .src([src + '/**/*.ts'])
+    .pipe(ts())
+    .pipe(gulp.dest(dist))
+);
 gulp.task('compile-json', () => copy('json'));
 gulp.task('compile-wxml', () => copy('wxml'));
 gulp.task('build', ext.map(ext => 'compile-' + ext));

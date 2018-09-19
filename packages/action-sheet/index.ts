5b374873021e4a8986a979d274795614d1922a9f
陈嘉涵 <chenjiahan@youzan.com>
2018-09-19 14:40:38 +0800

    ts

---
+++ packages/action-sheet/index.ts
@@ -0,0 +1,39 @@
+import { VantComponent } from '../common/component';
+
+VantComponent({
+  props: {
+    show: Boolean,
+    title: String,
+    cancelText: String,
+    actions: {
+      type: Array,
+      value: []
+    },
+    overlay: {
+      type: Boolean,
+      value: true
+    },
+    closeOnClickOverlay: {
+      type: Boolean,
+      value: true
+    }
+  },
+
+  methods: {
+    onSelect(event: Weapp.Event) {
+      const { index } = event.currentTarget.dataset;
+      const item = this.data.actions[index];
+      if (item && !item.disabled && !item.loading) {
+        this.$emit('select', item);
+      }
+    },
+
+    onCancel() {
+      this.$emit('cancel');
+    },
+
+    onClose() {
+      this.$emit('close');
+    }
+  }
+});
---
+++ packages/badge/index.ts
@@ -0,0 +1,27 @@
+import { VantComponent } from '../common/component';
+
+VantComponent({
+  relations: {
+    '../badge-group/index': {
+      type: 'ancestor'
+    }
+  },
+
+  props: {
+    info: Number,
+    title: String
+  },
+
+  methods: {
+    onClick() {
+      const group = this.getRelationNodes('../badge-group/index')[0];
+      if (group) {
+        group.setActive(this);
+      }
+    },
+
+    setActive(active) {
+      this.setData({ active });
+    }
+  }
+});

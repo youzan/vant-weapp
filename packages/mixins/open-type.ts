aa3d88307f907afc73d027ffb389bc901c675be4
陈嘉涵 <chenjiahan@youzan.com>
2018-09-27 11:42:00 +0800

    open type

---
+++ packages/mixins/open-type.ts
@@ -0,0 +1,27 @@
+export const openType = Behavior({
+  properties: {
+    openType: String
+  },
+
+  methods: {
+    bindGetUserInfo(event: Partial<Weapp.Event>) {
+      this.$emit('getuserinfo', event.detail);
+    },
+
+    bindContact(event: Partial<Weapp.Event>) {
+      this.$emit('contact', event.detail);
+    },
+
+    bindGetPhoneNumber(event: Partial<Weapp.Event>) {
+      this.$emit('getphonenumber', event.detail);
+    },
+
+    bindOpenSetting(event: Partial<Weapp.Event>) {
+      this.$emit('opensetting', event.detail);
+    },
+
+    bindError(event: Partial<Weapp.Event>) {
+      this.$emit('error', event.detail);
+    }
+  }
+});

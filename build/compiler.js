d53fc409866acc70184e379c393d2e5eb844f076
陈嘉涵 <chenjiahan@youzan.com>
2018-09-29 14:38:43 +0800

    [improvement] optimize common css

--- build/compiler.js
+++ build/compiler.js
@@ -1,10 +1,11 @@
 const gulp = require('gulp');
 const path = require('path');
-const babel = require('gulp-babel');
 const less = require('gulp-less');
-const postcss = require('gulp-postcss');
-const cssmin = require('gulp-clean-css');
+const babel = require('gulp-babel');
+const insert = require('gulp-insert');
 const rename = require('gulp-rename');
+const cssmin = require('gulp-clean-css');
+const postcss = require('gulp-postcss');
 
 const isProduction = process.env.NODE_ENV === 'production';
 const src = path.join(__dirname, '../packages');
@@ -21,6 +22,12 @@ gulp.task('compile-less', () => {
     .pipe(less())
     .pipe(postcss())
     .pipe(cssmin())
+    .pipe(insert.transform((contents, file) => {
+      if (!file.path.includes('packages/common')) {
+        contents = `@import '../common/index.wxss';` + contents;
+      }
+      return contents;
+    }))
     .pipe(
       rename(path => {
         path.extname = '.wxss';
---
+++ dist/checkbox-group/index.js
@@ -0,0 +1,39 @@
+import { VantComponent } from '../common/component';
+VantComponent({
+  relation: {
+    name: 'checkbox',
+    type: 'descendant',
+    linked: function linked(target) {
+      var _this$data = this.data,
+          value = _this$data.value,
+          disabled = _this$data.disabled;
+      target.setData({
+        value: value.indexOf(target.data.name) !== -1,
+        disabled: disabled || target.data.disabled
+      });
+    }
+  },
+  props: {
+    value: Array,
+    disabled: Boolean,
+    max: Number
+  },
+  watch: {
+    value: function value(_value) {
+      var children = this.getRelationNodes('../checkbox/index');
+      children.forEach(function (child) {
+        child.setData({
+          value: _value.indexOf(child.data.name) !== -1
+        });
+      });
+    },
+    disabled: function disabled(_disabled) {
+      var children = this.getRelationNodes('../checkbox/index');
+      children.forEach(function (child) {
+        child.setData({
+          disabled: _disabled || child.data.disabled
+        });
+      });
+    }
+  }
+});

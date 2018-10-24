51b05ed1a545fc6577ffbd17fc248d2fd1d02f65
rex-zsd <rexkaneki@gmail.com>
2018-10-24 21:58:23 +0800

    [build] 0.4.3

--- dist/goods-action-icon/index.js
+++ dist/goods-action-icon/index.js
@@ -1,7 +1,9 @@
-import { link } from '../mixins/link';
 import { VantComponent } from '../common/component';
+import { link } from '../mixins/link';
+import { button } from '../mixins/button';
+import { openType } from '../mixins/open-type';
 VantComponent({
-  mixins: [link],
+  mixins: [link, button, openType],
   props: {
     text: String,
     info: String,

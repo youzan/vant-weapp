975c64f48be65fbc1634bb7909bb879f0446f95e
rex-zsd <rexkaneki@gmail.com>
2018-10-31 09:51:10 +0800

    [build] 0.4.4

--- dist/search/index.js
+++ dist/search/index.js
@@ -4,8 +4,10 @@ VantComponent({
   classes: ['cancel-class'],
   props: {
     focus: Boolean,
+    error: Boolean,
     disabled: Boolean,
     readonly: Boolean,
+    inputAlign: String,
     showAction: Boolean,
     useActionSlot: Boolean,
     placeholder: String,

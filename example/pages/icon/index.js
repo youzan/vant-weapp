98abebeef1508b95589b8dc1dc68f913f8147fa7
陈嘉涵 <chenjiahan@youzan.com>
2018-12-04 14:31:54 +0800

    remove

--- example/pages/icon/index.js
+++ example/pages/icon/index.js
@@ -1,14 +1,7 @@
 import Page from '../../common/page';
 import config from './config';
 
-function repeat(arr) {
-  var result = [];
-  for (let i = 0; i < 500; i++) {
-    result.push(...arr);
-  }
-  return result;
-}
-const basic = repeat(config.basic.map(item => item.css));
+const basic = config.basic.map(item => item.css);
 const outline = config.outline.map(item => item.css);
 const filled = config.filled.map(item => item.css);
 

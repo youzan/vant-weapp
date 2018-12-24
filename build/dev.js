37b616251c2276ba29431d95a0e56d0bbc3c3b09
neverland <chenjiahan@buaa.edu.cn>
2018-12-24 20:23:09 +0800

    [breaking change] Icon: repaint all icons (#1141)

--- build/dev.js
+++ build/dev.js
@@ -3,7 +3,10 @@ const fs = require('fs-extra');
 const path = require('path');
 const serve = require('webpack-serve');
 const config = require('./webpack.doc.dev');
+const dist = path.join(__dirname, '../example/dist');
+const icons = path.join(__dirname, '../node_modules/@vant/icons');
 
-fs.removeSync(path.join(__dirname, '../example/dist'));
+fs.removeSync(dist);
+fs.copySync(icons, path.join(dist, '/@vant/icons'));
 
 serve({}, { config });

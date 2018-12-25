d9582105fb2c93063cea18d8e111f1cc8f342913
陈嘉涵 <chenjiahan@youzan.com>
2018-12-25 11:47:30 +0800

    [new feature] Slider: add button slot & drag event

--- example/pages/slider/index.js
+++ example/pages/slider/index.js
@@ -1,10 +1,20 @@
 import Page from '../../common/page';
 
 Page({
+  data: {
+    currentValue: 30
+  },
+
   onChange(event) {
     wx.showToast({
       icon: 'none',
       title: `当前值：${event.detail}`
+    });
+  },
+
+  onDrag(event) {
+    this.setData({
+      currentValue: event.detail.value
     });
   }
 });

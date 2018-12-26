youzan/vant-weapp

commit 696ae3c661f33933ab42ebd75ad5b0767826bdc2

fix(Stepper): fix minusDisabled、plusDisabled error when set value dynamic @rex-zsd (#1022)

packages/stepper/index.ts
@@ -14,6 +14,7 @@ VantComponent({
   ],
 
   props: {
+    value: Number,
     integer: Boolean,
     disabled: Boolean,
     disableInput: Boolean,
@@ -38,6 +39,14 @@ VantComponent({
 
     plusDisabled() {
       return this.data.disabled || this.data.value >= this.data.max;
+    }
+  },
+
+  watch: {
+    value(value) {
+      this.setData({
+        value: this.range(value)
+      });
     }
   },
 

commit 55e3778f0217e1bb8eef6c279b27fd7b229bbccc

[Bugfix] Stepper: allow delete input to empty
When the stepper has value '1' and the user try to edit the value
directly to '50', the input field stop user from delete '1' to ''.

The reason for the bug is `props: { value: Number }` trying to
convert empty string to 0, then `watch: { value() {} }` will set 0
to 1 automatically.

This fix try to:

* allow string as value to support empty string ''
* stop convert '' to 0 from watcher

The bug was introduced from 696ae3c.

Resolves: #1151

packages/stepper/index.ts
@@ -14,7 +14,7 @@ VantComponent({
   ],
 
   props: {
-    value: Number,
+    value: [Number, String],
     integer: Boolean,
     disabled: Boolean,
     asyncChange: Boolean,
@@ -45,6 +45,7 @@ VantComponent({
 
   watch: {
     value(value) {
+      if (value === '') return;
       this.set({
         value: this.range(value)
       });
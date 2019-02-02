705aa8c1df1ea25a5d28cf6a76fc373601da8e3e
rex <rexkaneki@gmail.com>
2019-02-02 16:18:12 +0800

    improvement: 更新weapp类型定义

--- types/weapp.d.ts
+++ types/weapp.d.ts
@@ -2,54 +2,81 @@ type BehaviorOptions = {
   [key: string]: any & ThisType<any>
 };
 
-type WX = {
-  [key: string]: any
-};
-
-declare const wx: WX;
-declare function Behavior(options: BehaviorOptions): void;
-declare function Component(options: any): void;
-declare function getCurrentPages(): Weapp.Page[];
+declare function Behavior(options: BehaviorOptions): void
+declare function Component(options: any): void
+declare function getCurrentPages(): Weapp.Page[]
 
 declare namespace Weapp {
   interface Component {
-    [key: string]: any;
-    getRelationNodes(selector: string): any[];
-    setData(data: any, callback?: Function): void;
+    [key: string]: any
+    getRelationNodes(selector: string): any[]
+    setData(data: any, callback?: Function): void
   }
 
   interface FormField {
     data: {
-      name: string;
-      value: any;
+      name: string
+      value: any
     }
   }
 
   interface Target {
-    id: string;
-    tagName: string;
-    dataset: any;
+    id: string
+    tagName: string
+    dataset: {
+      [key: string]: any
+    }
   }
 
   interface Event {
-    type: string;
-    detail: any;
-    timeStamp: number;
-    target: Target;
-    currentTarget: Target;
+    /**
+     * 代表事件的类型。
+     */
+    type: string
+    /**
+     * 页面打开到触发事件所经过的毫秒数。
+     */
+    timeStamp: number
+    /**
+     * 触发事件的源组件。
+     */
+    target: Target
+    /**
+     * 事件绑定的当前组件。
+     */
+    currentTarget: Target
+    /**
+     * 	额外的信息
+     */
+    detail: any
   }
 
   interface Touch {
-    identifier: number;
-    pageX: number;
-    pageY: number;
-    clientX: number;
-    clientY: number;
+    /**
+     * 触摸点的标识符
+     */
+    identifier: number
+    /**
+     * 距离文档左上角的距离，文档的左上角为原点 ，横向为X轴，纵向为Y轴
+     */
+    pageX: number
+    /**
+     * 距离文档左上角的距离，文档的左上角为原点 ，横向为X轴，纵向为Y轴
+     */
+    pageY: number
+    /**
+     * 距离页面可显示区域（屏幕除去导航条）左上角距离，横向为X轴，纵向为Y轴
+     */
+    clientX: number
+    /**
+     * 距离页面可显示区域（屏幕除去导航条）左上角距离，横向为X轴，纵向为Y轴
+     */
+    clientY: number
   }
 
   interface TouchEvent extends Event {
-    touches: Array<Touch>;
-    changedTouches: Array<Touch>;
+    touches: Array<Touch>
+    changedTouches: Array<Touch>
   }
 
   interface Page {

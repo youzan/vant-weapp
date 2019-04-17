9752c832d29ff903079b20c9b436f9462646b545
rex <rexkaneki@gmail.com>
2019-04-17 10:36:09 +0800

    [improvement] Picker: refactor component with movable-view

--- example/pages/picker/index.js
+++ example/pages/picker/index.js
@@ -1,25 +1,22 @@
 import Page from '../../common/page';
 import Toast from '../../dist/toast/toast';
 
+const citys = {
+  '浙江': ['杭州', { text: '宁波', disabled: true }, '温州', '嘉兴', '湖州'],
+  '福建': ['福州', '厦门', '莆田', '三明', '泉州']
+};
+
 Page({
   data: {
     column1: ['杭州', '宁波', '温州', '嘉兴', '湖州'],
-    column2: [
-      { text: '杭州', disabled: true },
-      { text: '宁波' },
-      { text: '温州' }
-    ],
-    column3: {
-      浙江: ['杭州', '宁波', '温州', '嘉兴', '湖州'],
-      福建: ['福州', '厦门', '莆田', '三明', '泉州']
-    },
+    column2: citys['浙江'],
     column4: [
       {
-        values: ['浙江', '福建'],
+        values: Object.keys(citys),
         className: 'column1'
       },
       {
-        values: ['杭州', '宁波', '温州', '嘉兴', '湖州'],
+        values: citys['浙江'],
         className: 'column2',
         defaultIndex: 2
       }
@@ -42,7 +39,6 @@ Page({
 
   onChange2(event) {
     const { picker, value } = event.detail;
-    picker.setColumnValues(1, this.data.column3[value[0]]);
-    getApp().picker = picker;
+    picker.setColumnValues(1, citys[value[0]]);
   }
 });

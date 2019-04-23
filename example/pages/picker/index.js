4570accf5a75a0aba2d4244a1013279c88ef1ace
rex-zsd <rexkaneki@gmail.com>
2019-04-23 17:09:42 +0800

    refactor(Picker): 回滚movable-view重构 @rex-zsd

--- example/pages/picker/index.js
+++ example/pages/picker/index.js
@@ -1,22 +1,25 @@
 import Page from '../../common/page';
 import Toast from '../../dist/toast/toast';
 
-const citys = {
-  '浙江': ['杭州', { text: '宁波', disabled: true }, '温州', '嘉兴', '湖州'],
-  '福建': ['福州', '厦门', '莆田', '三明', '泉州']
-};
-
 Page({
   data: {
     column1: ['杭州', '宁波', '温州', '嘉兴', '湖州'],
-    column2: citys['浙江'],
+    column2: [
+      { text: '杭州', disabled: true },
+      { text: '宁波' },
+      { text: '温州' }
+    ],
+    column3: {
+      浙江: ['杭州', '宁波', '温州', '嘉兴', '湖州'],
+      福建: ['福州', '厦门', '莆田', '三明', '泉州']
+    },
     column4: [
       {
-        values: Object.keys(citys),
+        values: ['浙江', '福建'],
         className: 'column1'
       },
       {
-        values: citys['浙江'],
+        values: ['杭州', '宁波', '温州', '嘉兴', '湖州'],
         className: 'column2',
         defaultIndex: 2
       }
@@ -39,6 +42,7 @@ Page({
 
   onChange2(event) {
     const { picker, value } = event.detail;
-    picker.setColumnValues(1, citys[value[0]]);
+    picker.setColumnValues(1, this.data.column3[value[0]]);
+    getApp().picker = picker;
   }
 });

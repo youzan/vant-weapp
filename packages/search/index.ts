81e74239c6e83991c496b7b726844b6a6eb61ab0
Jake <Jake.Laoyu@gmail.com>
2019-07-16 16:30:22 +0800

    [bugfix] Field: Modified value invalid

--- packages/search/index.ts
+++ packages/search/index.ts
@@ -37,9 +37,15 @@ VantComponent({
     },
 
     onCancel() {
-      this.set({ value: '' });
-      this.$emit('cancel');
-      this.$emit('change', '');
+      /**
+       * 修复修改输入框值时，输入框失焦和赋值同时触发，赋值失效
+       * // https://github.com/youzan/vant-weapp/issues/1768
+       */
+      setTimeout(() => {
+        this.set({ value: '' });
+        this.$emit('cancel');
+        this.$emit('change', '');
+      }, 200);
     },
 
     onSearch() {

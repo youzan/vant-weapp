44a134b577f8b35aa6640f8a1507cd4b3ba9eccc
陈嘉涵 <chenjiahan@youzan.com>
2019-08-02 16:43:47 +0800

    [Doc] add logo link

--- docs/src/doc.config.js
+++ docs/src/doc.config.js
@@ -13,7 +13,18 @@ export default {
       title: 'Vant Weapp',
       href: '#/'
     },
-    nav: {}
+    nav: {
+      logoLink: [
+        {
+          image: 'https://img.yzcdn.cn/vant/vant-o.svg',
+          url: 'https://youzan.github.io/vant'
+        },
+        {
+          image: 'https://img.yzcdn.cn/vant/logo/github.svg',
+          url: github
+        }
+      ]
+    }
   },
   nav: [
     {

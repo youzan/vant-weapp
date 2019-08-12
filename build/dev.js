2eba221484c0cb5a7cb6cf2ae744b951ec9b0825
rex-zsd <rexkaneki@gmail.com>
2019-08-12 20:21:01 +0800

    fix: fix missing @vant/icons when running dev @rex-zsd

--- build/dev.js
+++ build/dev.js
@@ -1,14 +1,9 @@
-const fs = require('fs-extra');
 const path = require('path');
 const serve = require('webpack-serve');
 const config = require('./webpack.doc.dev');
-const dist = path.join(__dirname, '../example/dist');
-const icons = path.join(__dirname, '../node_modules/@vant/icons');
 const { exec } = require('child_process');
 
 const gulpConfig = path.resolve(__dirname, './compiler.js');
-
-fs.copySync(icons, path.join(dist, '/@vant/icons'));
 
 serve({}, { config });
 

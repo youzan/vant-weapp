00f42dfb890fbf1e0311f9b4765bdab315cba3a6
rex-zsd <rexkaneki@gmail.com>
2019-08-27 15:36:09 +0800

    fix(TreeSelect): fix error when type of activeId is string or number @rex-zsd
    fix #1912

--- example/pages/tree-select/index.js
+++ example/pages/tree-select/index.js
@@ -3,27 +3,25 @@ import config from './config';
 
 Page({
   data: {
-    items: [
-      {
-        // 导航名称
-        text: '所有城市',
-        // 该导航下所有的可选项
-        children: [...config.pro1, ...config.pro2]
-      }, {
-        // 导航名称
-        text: config.pro1Name,
-        // 该导航下所有的可选项
-        children: config.pro1
-      }, {
-        text: config.pro2Name,
-        children: config.pro2
-      }, {
-        text: config.pro3Name,
-        disabled: true,
-        children: config.pro3
-      }
+    items: [{
+      text: '所有城市',
+      children: [...config.pro1, ...config.pro2]
+    }, {
+      text: config.pro1Name,
+      children: config.pro1
+    }, {
+      text: config.pro2Name,
+      children: config.pro2
+    }, {
+      text: config.pro3Name,
+      disabled: true,
+      children: config.pro3
+    }
     ],
-    mainActiveIndex: 0
+    mainActiveIndex: 0,
+    activeId: 0,
+    mainActiveIndexMulti: 0,
+    activeIdMulti: []
   },
 
   onClickNav({ detail }) {
@@ -33,23 +31,26 @@ Page({
   },
 
   onClickItem({ detail }) {
-    // 多选
-    if (!this.data.activeId) this.data.activeId = [];
+    const activeId = this.data.activeId === detail.id ? null : detail.id;
 
-    const idx = this.data.activeId.indexOf(detail.id);
+    this.setData({ activeId });
+  },
+
+  onClickNavMulti({ detail }) {
+    this.setData({
+      mainActiveIndexMulti: detail.index || 0
+    });
+  },
+
+  onClickItemMulti({ detail }) {
+    const { activeIdMulti } = this.data;
+    const idx = activeIdMulti.indexOf(detail.id);
     if (idx > -1) {
-      this.data.activeId.splice(idx, 1);
+      activeIdMulti.splice(idx, 1);
     } else {
-      this.data.activeId.push(detail.id);
+      activeIdMulti.push(detail.id);
     }
 
-/*
-    // 单选
-    this.data.activeId = this.data.activeId === detail.id ? null : detail.id;
-*/
-
-    this.setData({
-      activeId: this.data.activeId
-    });
+    this.setData({ activeIdMulti });
   }
 });

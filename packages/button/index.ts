66c74273935bdc0bf918ca17a878b70a1eb51c07
neverland <chenjiahan@youzan.com>
2019-09-03 16:42:24 +0800

    feat(Button): color prop support gradient color (#1944)

--- packages/button/index.ts
+++ packages/button/index.ts
@@ -7,9 +7,12 @@ VantComponent({
 
   classes: ['hover-class', 'loading-class'],
 
+  data: {
+    style: ''
+  },
+
   props: {
     icon: String,
-    color: String,
     plain: Boolean,
     block: Boolean,
     round: Boolean,
@@ -29,6 +32,32 @@ VantComponent({
     loadingSize: {
       type: String,
       value: '20px'
+    },
+    color: {
+      type: String,
+      observer(color: string) {
+        let style = '';
+
+        if (color) {
+          style += `color: ${this.data.plain ? color : 'white'};`;
+
+          if (!this.data.plain) {
+            // Use background instead of backgroundColor to make linear-gradient work
+            style += `background: ${color};`;
+          }
+
+          // hide border when color is linear-gradient
+          if (color.indexOf('gradient') !== -1) {
+            style += 'border: 0;';
+          } else {
+            style += `border-color: ${color};`;
+          }
+        }
+
+        if (style !== this.data.style) {
+          this.setData({ style });
+        }
+      }
     }
   },
 

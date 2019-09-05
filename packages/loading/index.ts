50478da7ef6d597b88434b0b96c8db99961a9214
neverland <chenjiahan@youzan.com>
2019-09-05 20:59:31 +0800

    feat(Loading): use addUnit method (#1969)

--- packages/loading/index.ts
+++ packages/loading/index.ts
@@ -1,14 +1,12 @@
 import { VantComponent } from '../common/component';
+import { addUnit } from '../common/utils';
 
-const defaultLoadingSize = '30px';
 VantComponent({
   props: {
     size: {
       type: String,
-      value: defaultLoadingSize,
-      observer: 'normalizeSize'
+      observer: 'setSizeWithUnit'
     },
-
     type: {
       type: String,
       value: 'circular'
@@ -20,20 +18,13 @@ VantComponent({
   },
 
   data: {
-    loadingSize: defaultLoadingSize
+    sizeWithUnit: '30px'
   },
 
   methods: {
-    // 支持 size="10" 和 size="10px" size="10rpx" 这些格式, 显然也不需要理会 size="abcpx"
-    normalizeSize(size: string | number): void {
-      const numberLike = (s: string) => !Number.isNaN(+s);
-      let loadingSize = size;
-      if (typeof size === 'number' || numberLike(size)) {
-        loadingSize = size + 'px';
-      }
-
-      this.setData({
-        loadingSize
+    setSizeWithUnit(size: string | number): void {
+      this.set({
+        sizeWithUnit: addUnit(size)
       });
     }
   }

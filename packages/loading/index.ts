f0d32f08a0473fe812f595e283669a3ecde7921a
thomasy <c6swift@gmail.com>
2019-09-05 20:50:33 +0800

    feat(Loading): 组件 size 支持 Number 格式 (#1952)

--- packages/loading/index.ts
+++ packages/loading/index.ts
@@ -1,11 +1,14 @@
 import { VantComponent } from '../common/component';
 
+const defaultLoadingSize = '30px';
 VantComponent({
   props: {
     size: {
       type: String,
-      value: '30px'
+      value: defaultLoadingSize,
+      observer: 'normalizeSize'
     },
+
     type: {
       type: String,
       value: 'circular'
@@ -13,6 +16,25 @@ VantComponent({
     color: {
       type: String,
       value: '#c9c9c9'
+    }
+  },
+
+  data: {
+    loadingSize: defaultLoadingSize
+  },
+
+  methods: {
+    // 支持 size="10" 和 size="10px" size="10rpx" 这些格式, 显然也不需要理会 size="abcpx"
+    normalizeSize(size: string | number): void {
+      const numberLike = (s: string) => !Number.isNaN(+s);
+      let loadingSize = size;
+      if (typeof size === 'number' || numberLike(size)) {
+        loadingSize = size + 'px';
+      }
+
+      this.setData({
+        loadingSize
+      });
     }
   }
 });

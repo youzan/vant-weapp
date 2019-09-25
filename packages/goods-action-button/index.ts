b69d11b5de92209374bb743472e40f566808744a
魏雪娥 <qie@youzan.com>
2019-09-25 17:57:24 +0800

    feat: 样式升级

--- packages/goods-action-button/index.ts
+++ packages/goods-action-button/index.ts
@@ -6,7 +6,13 @@ import { Weapp } from 'definitions/weapp';
 
 VantComponent({
   mixins: [link, button, openType],
-
+  relation: {
+    type: 'ancestor',
+    name: 'goods-action',
+    linked(parent) {
+      this.parent = parent;
+    }
+  },
   props: {
     text: String,
     color: String,
@@ -18,10 +24,28 @@ VantComponent({
     }
   },
 
+  mounted: function() {
+    this.updateStyle();
+  },
+
   methods: {
     onClick(event: Weapp.Event) {
       this.$emit('click', event.detail);
       this.jumpLink();
+    },
+    updateStyle() {
+      const parent = this.parent;
+      const { children = [] }= parent;
+      const index = children.indexOf(this);
+      const length = children.length;
+      let customClass = '';
+      if( index === 0 ) {
+        customClass += 'first';
+      }
+      if (index === length - 1) {
+        customClass += ' last';
+      }
+      this.setData({  customClass });
     }
   }
 });
--- packages/goods-action/index.ts
+++ packages/goods-action/index.ts
@@ -2,5 +2,18 @@ import { VantComponent } from '../common/component';
 import { safeArea } from '../mixins/safe-area';
 
 VantComponent({
-  mixins: [safeArea()]
+  mixins: [safeArea()],
+  relation: {
+    type: 'descendant',
+    name: 'goods-action-button',
+    linked(child) {
+      this.children.push(child);
+    },
+    unlinked(child) {
+      this.children = this.children.filter((item) => item !== child);
+    }
+  },
+  beforeCreate() {
+    this.children = [];
+  }
 });

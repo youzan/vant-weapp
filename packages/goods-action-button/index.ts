1a3332d36c0dcc87642c690a7304a5a66544b197
creeperdance <creeperdance@163.com>
2019-09-27 10:31:59 +0800

    feat(GoodsActionButton)：样式升级为圆角风格 (#2096)

--- packages/goods-action-button/index.ts
+++ packages/goods-action-button/index.ts
@@ -6,7 +6,13 @@ import { Weapp } from 'definitions/weapp';
 
 VantComponent({
   mixins: [link, button, openType],
-
+  relation: {
+    type: 'ancestor',
+    name: 'goods-action',
+    linked(parent) {
+      this.parent = parent;
+    }
+  },
   props: {
     text: String,
     color: String,
@@ -18,10 +24,31 @@ VantComponent({
     }
   },
 
+  mounted: function() {
+    this.updateStyle();
+  },
+
   methods: {
     onClick(event: Weapp.Event) {
       this.$emit('click', event.detail);
       this.jumpLink();
+    },
+    updateStyle() {
+      const parent = this.parent;
+      const { children = [] } = parent;
+      const index = children.indexOf(this);
+      const length = children.length;
+      let isFirst = false, isLast = false;
+      if ( index === 0 ) {
+        isFirst = true;
+      }
+      if (index === length - 1) {
+        isLast = true;
+      }
+      this.setData({
+        isFirst,
+        isLast
+      });
     }
   }
 });

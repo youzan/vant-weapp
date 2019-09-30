577cb8fa7222e76a9be12af69f440d86481f3271
neverland <chenjiahan@youzan.com>
2019-09-30 15:29:46 +0800

    docs(Checkbox): add custom shape demo (#2112)

--- example/pages/checkbox/index.js
+++ example/pages/checkbox/index.js
@@ -5,16 +5,13 @@ Page({
     checkbox1: true,
     checkbox2: true,
     checkbox3: true,
+    checkboxShape: true,
     list: ['a', 'b', 'c'],
     result: ['a', 'b'],
     result2: [],
     result3: [],
-    icon: {
-      normal:
-        'https://img.yzcdn.cn/public_files/2017/10/13/c547715be149dd3faa817e4a948b40c4.png',
-      active:
-        'https://img.yzcdn.cn/public_files/2017/10/13/793c77793db8641c4c325b7f25bf130d.png'
-    }
+    activeIcon: 'https://img.yzcdn.cn/vant/user-active.png',
+    inactiveIcon: 'https://img.yzcdn.cn/vant/user-inactive.png'
   },
 
   onChange(event) {

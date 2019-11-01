d98f6f19e8a2285b0aecccffc84faafdeb287cad
rex-zsd <rexkaneki@gmail.com>
2019-11-01 16:11:02 +0800

    feat(Tabbar & Tag): support css variables @rex-zsd

--- example/pages/tag/index.js
+++ example/pages/tag/index.js
@@ -1,3 +1,16 @@
 import Page from '../../common/page';
 
-Page();
+Page({
+  data: {
+    show: {
+      success: true,
+      primary: true
+    }
+  },
+
+  onClose(event) {
+    this.setData({
+      [`show.${event.target.id}`]: false
+    });
+  }
+});

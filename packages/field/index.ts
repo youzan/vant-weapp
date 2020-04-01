666103c3932b3daffac69e0ebb3d3a23ce59b3b4
rex-zsd <rexkaneki@gmail.com>
2020-04-01 12:47:33 +0800

    feat(Field): add new props auto-focus & disable-default-padding & cursor  add new event linechange
    fix #2189

--- packages/field/index.ts
+++ packages/field/index.ts
@@ -1,5 +1,6 @@
 import { VantComponent } from '../common/component';
 import { Weapp } from 'definitions/weapp';
+import { commonProps, inputProps, textareaProps } from './props';
 
 VantComponent({
   field: true,
@@ -7,63 +8,35 @@ VantComponent({
   classes: ['input-class', 'right-icon-class'],
 
   props: {
+    ...commonProps,
+    ...inputProps,
+    ...textareaProps,
     size: String,
     icon: String,
     label: String,
     error: Boolean,
-    fixed: Boolean,
-    focus: Boolean,
     center: Boolean,
     isLink: Boolean,
     leftIcon: String,
     rightIcon: String,
-    disabled: Boolean,
     autosize: [Boolean, Object],
-    readonly: Boolean,
+    readonly: {
+      type: Boolean,
+      observer: 'setShowClear'
+    },
     required: Boolean,
-    password: Boolean,
     iconClass: String,
-    clearable: Boolean,
+    clearable: {
+      type: Boolean,
+      observer: 'setShowClear'
+    },
     clickable: Boolean,
     inputAlign: String,
-    placeholder: String,
     customStyle: String,
-    confirmType: String,
-    confirmHold: Boolean,
-    holdKeyboard: Boolean,
     errorMessage: String,
     arrowDirection: String,
     showWordLimit: Boolean,
-    placeholderStyle: String,
     errorMessageAlign: String,
-    selectionEnd: {
-      type: Number,
-      value: -1
-    },
-    selectionStart: {
-      type: Number,
-      value: -1
-    },
-    showConfirmBar: {
-      type: Boolean,
-      value: true
-    },
-    adjustPosition: {
-      type: Boolean,
-      value: true
-    },
-    cursorSpacing: {
-      type: Number,
-      value: 50
-    },
-    maxlength: {
-      type: Number,
-      value: -1
-    },
-    type: {
-      type: String,
-      value: 'text'
-    },
     border: {
       type: Boolean,
       value: true
@@ -76,26 +49,33 @@ VantComponent({
 
   data: {
     focused: false,
+    innerValue: '',
+    showClear: false
+  },
+
+  created() {
+    this.value = this.data.value;
+    this.setData({ innerValue: this.value });
   },
 
   methods: {
     onInput(event: Weapp.Event) {
       const { value = '' } = event.detail || {};
 
-      this.setData({ value });
-
-      wx.nextTick(() => {
-        this.emitChange(value);
-      });
+      this.value = value;
+      this.setShowClear();
+      this.emitChange();
     },
 
     onFocus(event: Weapp.Event) {
-      this.setData({ focused: true });
+      this.focused = true;
+      this.setShowClear();
       this.$emit('focus', event.detail);
     },
 
     onBlur(event: Weapp.Event) {
-      this.setData({ focused: false });
+      this.focused = false;
+      this.setShowClear();
       this.$emit('blur', event.detail);
     },
 
@@ -104,21 +84,53 @@ VantComponent({
     },
 
     onClear() {
-      this.setData({ value: '' });
+      this.setData({ innerValue: '' });
+      this.value = '';
+      this.setShowClear();
 
       wx.nextTick(() => {
-        this.emitChange('');
+        this.emitChange();
         this.$emit('clear', '');
       });
     },
 
-    onConfirm() {
-      this.$emit('confirm', this.data.value);
+    onConfirm(event) {
+      const { value = '' } = event.detail || {};
+      this.value = value;
+      this.setShowClear();
+      this.$emit('confirm', value);
     },
 
-    emitChange(value) {
-      this.$emit('input', value);
-      this.$emit('change', value);
+    setValue(value) {
+      this.value = value;
+      this.setShowClear();
+
+      if (value === '') {
+        this.setData({ innerValue: '' });
+      }
+
+      this.emitChange();
+    },
+
+    onLineChange(event) {
+      this.$emit('linechange', event.detail);
+    },
+
+    onKeyboardHeightChange(event) {
+      this.$emit('keyboardheightchange', event.detail);
+    },
+
+    emitChange() {
+      this.$emit('input', this.value);
+      this.$emit('change', this.value);
+    },
+
+    setShowClear() {
+      const { clearable, readonly } = this.data;
+      const { focused, value } = this;
+      this.setData({
+        showClear: clearable && focused && !!value && !readonly
+      });
     },
 
     noop() {}

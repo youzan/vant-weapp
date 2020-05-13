b0fea0ccfc49092ca2bd543f177f2ee15cbd7fca
rex <rexkaneki@gmail.com>
2020-05-13 17:54:51 +0800

    fix(goods-action-button): rerender style after link change
    fix #3138

--- packages/goods-action/index.ts
+++ packages/goods-action/index.ts
@@ -5,11 +5,32 @@ VantComponent({
     type: 'descendant',
     name: 'goods-action-button',
     current: 'goods-action',
+    linked() {
+      this.updateStyle();
+    },
+    unlinked() {
+      this.updateStyle();
+    },
+    linkChanged() {
+      this.updateStyle();
+    },
   },
   props: {
     safeAreaInsetBottom: {
       type: Boolean,
       value: true,
+    },
+  },
+
+  methods: {
+    updateStyle() {
+      wx.nextTick(() => {
+        this.children.forEach(
+          (child: WechatMiniprogram.Component.TrivialInstance) => {
+            child.updateStyle();
+          }
+        );
+      });
     },
   },
 });

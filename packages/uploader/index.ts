8806e389a6dbb4e3e26bbc2f21bdff0d68436784
王颖博 <wangyingbo528@yeah.net>
2020-06-12 10:47:48 +0800

    Update index.ts

--- packages/uploader/index.ts
+++ packages/uploader/index.ts
@@ -77,6 +77,10 @@ VantComponent({
           typeof item.isImage === 'undefined'
             ? isImageFile(item)
             : item.isImage,
+        deletable:
+          typeof item.deletable === 'undefined'
+            ? true
+            : item.deletable,
       }));
       this.setData({ lists, isInCount: lists.length < maxCount });
     },

b9a7a3e8ee1534124c3421d81c86626e07890a05
rex <rexkaneki@gmail.com>
2020-07-14 23:47:18 +0800

    feat(notice-bar): add new prop background & add new event close
    fix #3380

--- example/pages/notice-bar/index.js
+++ example/pages/notice-bar/index.js
@@ -2,12 +2,7 @@ import Page from '../../common/page';
 
 Page({
   data: {
-    text: '足协杯战线连续第2年上演广州德比战，上赛季半决赛上恒大以两回合5-3的总比分淘汰富力。',
-    speedValue: 80
-  },
-  onChange(event) {
-    this.setData({
-      speedValue: event.detail
-    });
+    text: '在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。',
+    shortText: '技术是开发它的人的共同灵魂。'
   }
 });

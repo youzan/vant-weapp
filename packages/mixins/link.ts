6e804f80a08e93422941d860c6eb17cb9f5c1423
九旬大爷的梦 <1160030724@qq.com>
2020-12-03 11:02:13 +0800

    Update link.ts
    fix(mixins): 页面栈上限后导致的跳转失败

--- packages/mixins/link.ts
+++ packages/mixins/link.ts
@@ -10,8 +10,12 @@ export const link = Behavior({
   methods: {
     jumpLink(urlKey = 'url') {
       const url = this.data[urlKey];
-      if (url) {
-        wx[this.data.linkType]({ url });
+      if (url) {        
+        if (this.data.linkType === 'navigateTo' && getCurrentPages().length > 9) {
+          wx.redirectTo({ url });
+        } else {
+          wx[this.data.linkType]({ url });
+        }
       }
     },
   },

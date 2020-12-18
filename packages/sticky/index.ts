d5e4cf9ff069671cc593cc0a1daf22bd522a9da0
rex <rexkaneki@gmail.com>
2020-12-18 16:10:11 +0800

    perf(sticky): avoid set data if nothing change (#3879)

--- packages/sticky/index.ts
+++ packages/sticky/index.ts
@@ -108,7 +108,9 @@ VantComponent({
           return prev;
         }, {});
 
-        this.setData(diff);
+        if (Object.keys(diff).length > 0) {
+          this.setData(diff);
+        }
 
         this.$emit('scroll', {
           scrollTop: this.scrollTop,

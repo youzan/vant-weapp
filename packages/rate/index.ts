ff738940f9b1712d0d06f10ab0250ebd9782fe9a
nemo-shen <kienshin626@gmail.com>
2021-05-07 23:55:02 +0800

    fix(Rate): support touch move and selected half star

--- packages/rate/index.ts
+++ packages/rate/index.ts
@@ -85,8 +85,9 @@ VantComponent({
 
       getAllRect(this, '.van-rate__icon').then((list) => {
         const target = list
-          .sort((item) => item.right - item.left)
+          .sort((cur, next) => cur.dataset.score - next.dataset.score)
           .find((item) => clientX >= item.left && clientX <= item.right);
+
         if (target != null) {
           this.onSelect({
             ...event,

2ff2630b7bb2fbddeaf0402ea22ae0462f596333
nemo-shen <kienshin626@gmail.com>
2021-08-27 09:58:03 +0800

    fix(Sticky): Cannot read property 'top' of null

--- packages/sticky/index.ts
+++ packages/sticky/index.ts
@@ -89,6 +89,7 @@ VantComponent({
       }
 
       getRect(this, ROOT_ELEMENT).then((root) => {
+
         if (offsetTop >= root.top) {
           this.setDataAfterDiff({ fixed: true, height: root.height });
           this.transform = 0;

cc2cd71e4ac0ef05a88a055953bc5d9e3a3a1e5d
nemo-shen <kienshin626@gmail.com>
2021-12-01 23:59:53 +0800

    test(Card): add demo test

--- example/pages/card/index.js
+++ example/pages/card/index.js
@@ -1,7 +1,3 @@
 import Page from '../../common/page';
 
-Page({
-  data: {
-    imageURL: 'https://img.yzcdn.cn/upload_files/2017/07/02/af5b9f44deaeb68000d7e4a711160c53.jpg'
-  }
-});
+Page();
---
+++ packages/card/demo/index.ts
@@ -0,0 +1,8 @@
+import { VantComponent } from '../../common/component';
+
+VantComponent({
+  data: {
+    imageURL:
+      'https://img.yzcdn.cn/upload_files/2017/07/02/af5b9f44deaeb68000d7e4a711160c53.jpg',
+  },
+});

3edbcdce538cb9119e26d0202b69bf202d3aa8f9
nemo-shen <kienshin626@gmail.com>
2021-12-01 22:35:29 +0800

    test(Switch): add demo test

--- example/pages/switch/index.js
+++ example/pages/switch/index.js
@@ -1,22 +1,3 @@
 import Page from '../../common/page';
-import Dialog from '../../dist/dialog/dialog';
 
-Page({
-  data: {
-    checked: true,
-    checked2: true
-  },
-
-  onChange({ detail }) {
-    this.setData({ checked: detail });
-  },
-
-  onChange2({ detail }) {
-    Dialog.confirm({
-      title: '提示',
-      message: '是否切换开关？'
-    }).then((res) => {
-      this.setData({ checked2: detail });
-    });
-  }
-});
+Page();

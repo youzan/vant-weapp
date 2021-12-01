a452e8449a2c3b9825d0a81ca5bac1abce2cfc80
nemo-shen <kienshin626@gmail.com>
2021-12-01 23:56:35 +0800

    test(Grid): add demo test

---
+++ packages/grid/demo/index.ts
@@ -0,0 +1,10 @@
+import { VantComponent } from '../../common/component';
+
+VantComponent({
+  data: {
+    array3: [0, 1, 2],
+    array4: [0, 1, 2, 3],
+    array6: [0, 1, 2, 3, 4, 5],
+    array8: [0, 1, 2, 3, 4, 5, 6, 7],
+  },
+});

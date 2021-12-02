24017c1c8808da883f94c782a34ea4a9eb1a2c43
nemo-shen <kienshin626@gmail.com>
2021-12-02 23:36:03 +0800

    test(Image): add demo test

--- example/pages/image/index.js
+++ example/pages/image/index.js
@@ -1,16 +1,3 @@
 import Page from '../../common/page';
 
-Page({
-  data: {
-    fits: [
-      'contain',
-      'cover',
-      'fill',
-      'none',
-      'scale-down',
-      'widthFix',
-      'heightFix'
-    ],
-    src: 'https://img.yzcdn.cn/vant/cat.jpeg'
-  }
-});
+Page();

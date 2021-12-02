46bcfc28196768359d73ecb14c17afa13ece9f62
nemo-shen <kienshin626@gmail.com>
2021-12-02 23:36:03 +0800

    test(Image): add demo test

---
+++ packages/image/demo/index.ts
@@ -0,0 +1,16 @@
+import { VantComponent } from '../../common/component';
+
+VantComponent({
+  data: {
+    fits: [
+      'contain',
+      'cover',
+      'fill',
+      'none',
+      'scale-down',
+      'widthFix',
+      'heightFix',
+    ],
+    src: 'https://img.yzcdn.cn/vant/cat.jpeg',
+  },
+});

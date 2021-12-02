13df6519a45a2971290c8964b4267d2f0c6ae3f1
nemo-shen <kienshin626@gmail.com>
2021-12-02 23:28:37 +0800

    test(Icon): add demo test

--- example/pages/icon/index.js
+++ example/pages/icon/index.js
@@ -1,17 +1,3 @@
-import icons from '../../dist/@vant/icons/src/config';
 import Page from '../../common/page';
 
-Page({
-  data: {
-    icons,
-    active: 0,
-    demoIcon: 'chat-o',
-    demoImage: 'https://b.yzcdn.cn/vant/icon-demo-1126.png',
-  },
-
-  onSwitch(event) {
-    this.setData({
-      active: event.detail.index
-    });
-  }
-});
+Page();

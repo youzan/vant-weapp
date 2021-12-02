76ba81a6a7da127a3098546cde37e93dc422532f
nemo-shen <kienshin626@gmail.com>
2021-12-02 23:51:17 +0800

    test(Search): add demo test

---
+++ packages/search/demo/index.ts
@@ -0,0 +1,47 @@
+import { VantComponent } from '../../common/component';
+
+VantComponent({
+  data: {
+    value: '',
+  },
+
+  methods: {
+    onChange(e) {
+      this.setData({
+        value: e.detail,
+      });
+    },
+
+    onSearch() {
+      if (this.data.value) {
+        wx.showToast({
+          title: '搜索：' + this.data.value,
+          icon: 'none',
+        });
+      }
+    },
+
+    onClick() {
+      if (this.data.value) {
+        wx.showToast({
+          title: '搜索：' + this.data.value,
+          icon: 'none',
+        });
+      }
+    },
+
+    onCancel() {
+      wx.showToast({
+        title: '取消',
+        icon: 'none',
+      });
+    },
+
+    onClear() {
+      wx.showToast({
+        title: '清空',
+        icon: 'none',
+      });
+    },
+  },
+});

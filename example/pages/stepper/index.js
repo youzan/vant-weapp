ce8955560e04cab25f66a7f8462e965cc0df4606
nemo-shen <kienshin626@gmail.com>
2021-12-02 23:59:47 +0800

    test(Stepper): add demo test

--- example/pages/stepper/index.js
+++ example/pages/stepper/index.js
@@ -1,19 +1,3 @@
 import Page from '../../common/page';
-import Toast from '../../dist/toast/toast';
 
-Page({
-  data: {
-    value: 1
-  },
-
-  onChange(event) {
-    Toast.loading({
-      forbidClick: true
-    });
-
-    setTimeout(() => {
-      Toast.clear();
-      this.setData({ value: event.detail });
-    }, 500);
-  }
-});
+Page();

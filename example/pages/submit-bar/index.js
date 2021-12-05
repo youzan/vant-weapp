fdb58a2654b71f050820a079edb987cfaca75b08
nemo-shen <kienshin626@gmail.com>
2021-12-06 00:16:43 +0800

    test(SubmitBar): add demo test

--- example/pages/submit-bar/index.js
+++ example/pages/submit-bar/index.js
@@ -1,11 +1,3 @@
 import Page from '../../common/page';
-import Toast from '../../dist/toast/toast';
 
-Page({
-  onClickButton() {
-    Toast('点击按钮');
-  },
-  onClickLink() {
-    Toast('修改地址');
-  }
-});
+Page();

01559f7a4289bc75f85b050a6c08f3bff32aacf1
nemo-shen <kienshin626@gmail.com>
2021-12-12 22:58:57 +0800

    test(Field): add demo test

--- example/pages/field/index.js
+++ example/pages/field/index.js
@@ -1,21 +1,3 @@
 import Page from '../../common/page';
 
-Page({
-  data: {
-    sms: '',
-    value: '',
-    password: '',
-    username: '',
-    username2: '',
-    username3: '',
-    message: '',
-    phone: '1365577'
-  },
-
-  onClickIcon() {
-    wx.showToast({
-      icon: 'none',
-      title: '点击图标'
-    });
-  }
-});
+Page();

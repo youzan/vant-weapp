2f7c6917a690332a9c0e7d514468c5be3f597286
nemo-shen <kienshin626@gmail.com>
2021-12-12 22:45:28 +0800

    test(NoticeBar): add demo test

--- example/pages/notice-bar/index.js
+++ example/pages/notice-bar/index.js
@@ -1,8 +1,3 @@
 import Page from '../../common/page';
 
-Page({
-  data: {
-    text: '在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。',
-    shortText: '技术是开发它的人的共同灵魂。',
-  },
-});
+Page();
---
+++ packages/notice-bar/demo/index.ts
@@ -0,0 +1,8 @@
+import { VantComponent } from '../../common/component';
+
+VantComponent({
+  data: {
+    text: '在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。',
+    shortText: '技术是开发它的人的共同灵魂。',
+  },
+});

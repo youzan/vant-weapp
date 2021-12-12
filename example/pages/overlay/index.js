40349869e5649fa357b9ebb9f3dc5259702ed677
nemo-shen <kienshin626@gmail.com>
2021-12-12 22:38:21 +0800

    test(Overlay): add demo test

--- example/pages/overlay/index.js
+++ example/pages/overlay/index.js
@@ -1,26 +1,3 @@
 import Page from '../../common/page';
 
-Page({
-  data: {
-    show: false,
-    showEmbedded: false
-  },
-
-  onClickShow() {
-    this.setData({ show: true });
-  },
-
-  onClickHide() {
-    this.setData({ show: false });
-  },
-
-  onClickShowEmbedded() {
-    this.setData({ showEmbedded: true });
-  },
-
-  onClickHideEmbedded() {
-    this.setData({ showEmbedded: false });
-  },
-
-  noop() {}
-});
+Page();

6899aeeecae19fc337334a1e7902e2d674e3e11f
chenjiahan <chenjiahan.jait@bytedance.com>
2022-05-03 10:35:32 +0800

    docs: should use v2 demo in simulator

--- vant.config.mjs
+++ vant.config.mjs
@@ -12,7 +12,7 @@ export default {
     description: '轻量、可靠的小程序 UI 组件库',
     logo: 'https://img.yzcdn.cn/vant/logo.png',
     simulator: {
-      url: 'https://vant-contrib.gitee.io/vant/mobile.html?weapp=1',
+      url: 'https://vant-contrib.gitee.io/vant/v2/mobile.html?weapp=1',
       routeMapper: (path) => {
         const map = {
           '/common': '/style',

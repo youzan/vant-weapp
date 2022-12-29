0b3789fdca41e9e2bd3856cbebbaa27060f92080
landluck <landl_ww@163.com>
2022-12-29 19:26:48 +0800

    feat(image): add webp property support

--- packages/image/index.ts
+++ packages/image/index.ts
@@ -28,6 +28,10 @@ VantComponent({
       type: String,
       value: 'fill',
     },
+    webp: {
+      type: Boolean,
+      value: false,
+    },
     showError: {
       type: Boolean,
       value: true,

46a1311a86cfd8388c775baaa631d8990b00ff4d
liu <8185129+three-gold-bows@user.noreply.gitee.com>
2024-01-30 11:29:18 +0800

    fix(van-toast): 解决Toast.clear失效问题，van-toast的数据应该定义在data中，而非props。

--- packages/toast/index.ts
+++ packages/toast/index.ts
@@ -1,29 +1,18 @@
 import { VantComponent } from '../common/component';
 
 VantComponent({
-  props: {
-    show: Boolean,
-    mask: Boolean,
-    message: String,
-    forbidClick: Boolean,
-    zIndex: {
-      type: Number,
-      value: 1000,
-    },
-    type: {
-      type: String,
-      value: 'text',
-    },
-    loadingType: {
-      type: String,
-      value: 'circular',
-    },
-    position: {
-      type: String,
-      value: 'middle',
-    },
+  data: {
+    show: false,
+    type: 'text',
+    mask: false,
+    message: '',
+    zIndex: 1000,
+    duration: 2000,
+    position: 'middle',
+    forbidClick: false,
+    loadingType: 'circular',
+    selector: '#van-toast',
   },
-
   methods: {
     // for prevent touchmove
     noop() {},
